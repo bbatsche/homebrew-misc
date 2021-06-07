6be60fbbf696a39d7d35501c4c421ae627589106
Ben Batschelet <batschel@adobe.com>
2021-06-07 16:30:01 -0500

    Add formulae

---
+++ Formula/fisherman.rb
@@ -0,0 +1,17 @@
+class Fisherman < Formula
+  desc "fish plugin manager"
+  homepage "https://github.com/jorgebucaran/fisher"
+
+  url "https://github.com/jorgebucaran/fisher/archive/refs/tags/4.3.0.tar.gz"
+  sha256 "6235cfc636c8d52f11feca9f4931656a9c6602659b06df8dba5a3606d37f8c28"
+
+  head "https://github.com/fisherman/fisherman.git"
+
+  depends_on "fish"
+
+  def install
+    fish_function.install "fisher.fish" => "fisher.fish"
+
+    ohai "You may need to restart any open terminal sessions for changes to take effect."
+  end
+end
---
+++ Formula/unrpyc.rb
@@ -0,0 +1,17 @@
+class Unrpyc < Formula
+  desc "Ren'py script decompiler"
+  homepage "https://github.com/CensoredUsername/unrpyc"
+
+  url "https://github.com/CensoredUsername/unrpyc/archive/refs/tags/v1.1.6.tar.gz"
+  sha256 "d2285cfb5d4d5e5294b8731b1b2fe5cae1f70f68b7573f16f2561b6357e61a15"
+
+  head "https://github.com/CensoredUsername/unrpyc.git"
+
+  def install
+    prefix.install "unrpyc.py"
+    prefix.install "deobfuscate.py"
+    prefix.install Dir["decompiler"]
+
+    bin.install_symlink "#{prefix}/unrpyc.py" => "unrpyc"
+  end
+end
