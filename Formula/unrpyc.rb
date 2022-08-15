a8b933868b6dfa8db779cd937f1ba9527ff9fa6a
Ben Batschelet <bartleby84@gmail.com>
2022-08-15 11:55:27 -0500

    Update unrpyc

--- Formula/unrpyc.rb
+++ Formula/unrpyc.rb
@@ -2,10 +2,12 @@ class Unrpyc < Formula
   desc "Ren'py script decompiler"
   homepage "https://github.com/CensoredUsername/unrpyc"
 
-  url "https://github.com/CensoredUsername/unrpyc/archive/refs/tags/v1.1.6.tar.gz"
-  sha256 "d2285cfb5d4d5e5294b8731b1b2fe5cae1f70f68b7573f16f2561b6357e61a15"
+  url "https://github.com/CensoredUsername/unrpyc/archive/refs/tags/v1.1.8.tar.gz"
+  sha256 "ab2e2fb2a38ee5ad465dd7a7f7dd749cef52a3a24ee5a8b4a27c54754921bb4a"
 
   head "https://github.com/CensoredUsername/unrpyc.git"
+
+  depends_on "python@2.7"
 
   def install
     prefix.install "unrpyc.py"
